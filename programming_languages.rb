59dd27b41bbbd768a464318ab554d795746b2643
Maija Barrins <maija.barrins@gmail.com>
2019-04-29 11:29:54 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
-  # your code here
+	new_languages = languages.values.map {|language| language}.inject(:merge).to_h
+	new_languages.each do |new_language, details|
+    binding.pry
+		languages.each do |style, language|
+			if details.has_key?(:style)
+				new_languages[new_language][details][:style] << style.to_s
+			else
+				new_languages[new_language][details][:style] = style.to_s
+			end 
+		end
+	end
+	new_languages
 end
