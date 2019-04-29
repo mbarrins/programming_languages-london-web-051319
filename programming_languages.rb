6650dafa03c1e8edc1691b1fcb18c6e23091846f
Maija Barrins <maija.barrins@gmail.com>
2019-04-29 11:42:00 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,9 +3,9 @@ require "pry"
 def reformat_languages(languages)
 	new_languages = languages.values.map {|language| language}.inject(:merge).to_h
 	new_languages.each do |new_language, details|
-    binding.pry
 		languages.each do |new_style, language|
 			if details.has_key?(:style)
+        binding.pry
 				new_languages[new_language][details][:style] << new_style.to_s
 			else
 				new_languages[new_language][details][:style] = new_style.to_s
